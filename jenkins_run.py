4bbdcde36d7ac724c1fe66e4ffea7a2c257bdab8
chenshijun <953841463@qq.com>
2020-12-16 10:54:27 +0800

    peoject init

--- jenkins_run.py
+++ jenkins_run.py
@@ -7,17 +7,14 @@ import pytest
 currentPath = os.path.dirname(os.path.abspath(__file__))
 
 
-def run_test(one=True):
-    if one:
-        date = time.strftime("%Y-%m-%d", time.localtime())
-    else:
-        date = time.strftime("%Y-%m-%d_%H_%M_%S", time.localtime())
+def run_test():
+    date = time.strftime("%Y-%m-%d", time.localtime())
     report_path = os.path.join(currentPath, 'report', str(date))
     if os.path.exists(report_path):
         shutil.rmtree(report_path)
     test_ed = os.path.join(currentPath, 'TestCase')  # 执行文件夹下面的所有测试用例
     pytest.main([test_ed, '--alluredir=%s' % report_path, '-o log_cli=true -o log_cli_level=INFO'])
-    os.system('cp ./report/environment.properties {}/environment.properties'.format(report_path))
+    # os.system('cp ./report/environment.properties {}/environment.properties'.format(report_path))
 
     print("**************** done ****************")
 
