783e1fdde653d6f922eabfc933f7107085cf50ad
chenshijun <953841463@qq.com>
2020-12-16 10:44:14 +0800

    peoject init

--- jenkins_run.py
+++ jenkins_run.py
@@ -3,7 +3,6 @@ import shutil
 import time
 
 import pytest
-from util.reportHelp import set_allure_environment, set_allure_report_title, set_allure_logo, set_allure_logo_text
 
 currentPath = os.path.dirname(os.path.abspath(__file__))
 
@@ -19,11 +18,7 @@ def run_test(one=True):
     test_ed = os.path.join(currentPath, 'TestCase')  # 执行文件夹下面的所有测试用例
     pytest.main([test_ed, '--alluredir=%s' % report_path, '-o log_cli=true -o log_cli_level=INFO'])
     os.system('cp ./report/environment.properties {}/environment.properties'.format(report_path))
-    os.system('allure generate %s -o %s/html --clean' % (report_path, report_path))
-    set_allure_report_title(report_path, {"reportName": "第一版本"})
-    set_allure_environment(report_path)
-    set_allure_logo(report_path)
-    set_allure_logo_text(report_path, "API")
+
     print("**************** done ****************")
 
 
